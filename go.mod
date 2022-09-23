2a67cf05a7e8fec5bd3582d0a0775cf4a0d798ea
zachfurie <zachfurie0608@gmail.com>
2022-09-23 19:38:28 -0400

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zachfurie/gograd
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	gograd_1d "github.com/zachfurie/gograd/gograd_1d"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	gograd_1d.Run()
+}
