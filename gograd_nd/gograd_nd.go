3b2261ad45182dd741d6155709c843879774ca0d
zachfurie <zachfurie0608@gmail.com>
2022-10-14 20:53:02 -0400

    nd tensor implementation and transpose

--- gograd_nd/gograd_nd.go
+++ gograd_nd/gograd_nd.go
@@ -1,5 +1,7 @@
 package gograd_nd
 
+import "fmt"
+
 // ----------------------------------- Notes: -----------------------------------
 
 // Need to refactor code to use tensornd structs instead of tensor structs.
@@ -23,18 +25,49 @@ type tensor struct {
 	data   []float64 // all tensor data in a single slice
 	dimlen []int     // [i] = length of dimension i
 	diminc []int     // [i] = increment of dimension i
-	l      int       // = len(data)
-	dims   int       // = len(dimlen)  = len(diminc)
 }
 
 //  ----------------------------------- Auxiliary Functions:  -----------------------------------
 
+// NOTE: not currently checking for errors in these functions!!!
+
 func (t *tensor) get(ind []int) float64 {
-	// assert len(ind) = t.dims
-	ret := 0.
-	return ret
+	index := 0
+	for i, x := range ind {
+		index += t.diminc[i] * x
+	}
+	return t.data[index]
 }
 
 func (t *tensor) shape() []int {
 	return t.dimlen
+}
+
+func (t *tensor) transpose(dim1 int, dim2 int) {
+	t.diminc[dim1], t.diminc[dim2] = t.diminc[dim2], t.diminc[dim1]
+	t.dimlen[dim1], t.dimlen[dim2] = t.dimlen[dim2], t.dimlen[dim1]
+}
+
+// ----------------------------------- TESTING (will be removed when done) -----------------------------------
+
+func Test() {
+	data := []float64{1, 2, 3, 4}
+	dl := []int{4, 2}
+	di := []int{2, 1}
+	a := tensor{data, dl, di}
+	g := []int{0, 0}
+	gg := []int{0, 1}
+	fmt.Println(a.get(g), a.get(gg))
+	g = []int{1, 0}
+	gg = []int{1, 1}
+	fmt.Println(a.get(g), a.get(gg))
+	fmt.Println("transpose")
+	a.transpose(0, 1)
+	g = []int{0, 0}
+	gg = []int{0, 1}
+	fmt.Println(a.get(g), a.get(gg))
+	g = []int{1, 0}
+	gg = []int{1, 1}
+	fmt.Println(a.get(g), a.get(gg))
+
 }
--- main.go
+++ main.go
@@ -7,7 +7,8 @@ import (
 	"time"
 
 	// gograd_1d "github.com/zachfurie/gograd/gograd_1d"
-	gograd_2d "github.com/zachfurie/gograd/gograd_2d"
+	// gograd_2d "github.com/zachfurie/gograd/gograd_2d"
+	gograd_nd "github.com/zachfurie/gograd/gograd_nd"
 )
 
 func main() {
@@ -16,5 +17,7 @@ func main() {
 	fmt.Println("using ", runtime.GOMAXPROCS(numCPUs), " cores")
 	rand.Seed(time.Now().UnixNano())
 	fmt.Println("------------------------------------------------------------------------")
-	gograd_2d.Simple()
+	// gograd_2d.Simple()
+	gograd_nd.Test()
+
 }
